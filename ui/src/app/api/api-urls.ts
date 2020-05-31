88a8eb385a14967fd7e5985d005f2b55356f4359
Oliver Chmelicky <oliver.chmelicky@gmail.com>
2020-05-31 18:33:40 +0200

    nasadena verzia tam

--- ui/src/app/api/api-urls.ts
+++ ui/src/app/api/api-urls.ts
@@ -1,5 +1,5 @@
 export const ApisModel = {
-  apiUrl: 'http://localhost:8081',//'http://localhost:8081',  https://litter3-olo-gcp.appspot.com
+  apiUrl: 'https://litter3-olo-gcp.appspot.com',//'http://localhost:8081',  https://litter3-olo-gcp.appspot.com
   user: 'users',
   society: 'societies',
   trash: 'trash',
